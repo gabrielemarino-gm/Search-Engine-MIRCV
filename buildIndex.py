345a756dbfa561a69a0cb7ecea7da2c4e9fe6b78
Matteo Pasqualetti <matteo.pasqualetti@libero.it>
2023-10-07 17:47:00 +0200

    Index Creation File - Base

--- buildIndex.py
+++ buildIndex.py
@@ -1,1 +1,26 @@
+# --------------------PACKAGES--------------------
 from icecream import ic
+from typing import List, Tuple
+import sys
+
+# ----------------GLOBAL VARIABLES----------------
+# TODO -> Pick those from XML or input parameters
+
+CORPUS_PATH = "/data"
+
+
+# --------------MAIN-ONLY FUNCTIONS---------------
+def getparams(lst: List[str]) -> Tuple[bool, str]:
+    # TODO -> By now, debug mode is set to true
+    debug = True
+    mode = "TFIDF"
+    # TODO -> Inspect lst and get info about index mode and debug active/deactive
+    return debug, mode
+
+
+# ----------------STARTING POINT------------------
+dbg, _ = getparams(sys.argv)
+
+if not dbg: ic.disable()
+
+ic()
