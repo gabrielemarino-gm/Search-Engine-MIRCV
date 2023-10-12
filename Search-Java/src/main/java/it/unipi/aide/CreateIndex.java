c40c3e63ad1788d20614adac8557efca17797cf3
Matteo Pasqualetti <matteo.pasqualetti@libero.it>
2023-10-12 16:04:00 +0200

    Fixed Maven

--- Search-Java/src/main/java/it/unipi/aide/CreateIndex.java
+++ Search-Java/src/main/java/it/unipi/aide/CreateIndex.java
@@ -1,9 +1,51 @@
 package it.unipi.aide;
 
-public class CreateIndex
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class CreateIndex {
+    public static void main(String[] args) {
+        boolean DEBUG = false;
+        String MODE = "TFIDF";
+
+        // Preprocessing test
+        /*List<String> terms = runPreprocessing();
+        System.out.println(Arrays.toString(terms.toArray()));*/
+
+        // Index building
+        buildIndex();
+    }
+
+    /*private static List<String> runPreprocessing() {
+
+        List<String> terms = null;
+
+        try
+        {
+            // File access
+            String currentDirectory = System.getProperty("user.dir");
+
+            String filePath = currentDirectory + "/src/main/java/it/unipi/aide/config/prova.txt";
+            String text = new String(Files.readAllBytes(Paths.get(filePath)));
+
+            // Preprocessing application
+            Preprocesser p = new Preprocesser(true);
+            terms = p.process(text);
+        }
+        catch (Exception e)
+        {
+            System.err.println(e.getMessage());
+            System.exit(1);
+        }
+
+        return terms;
+    }*/
+
+    private static void buildIndex() {
+
+        String currentDirectory = System.getProperty("user.dir");
+        System.out.println(currentDirectory);
+        String inputPath = currentDirectory + "/src/main/java/it/unipi/aide/mini_collection.tsv";
+        String outputPath = currentDirectory + "/src/main/java/it/unipi/aide/data";
+
+//        SPIMI s = new SPIMI(inputPath, outputPath, 75, true);
+//        s.algorithm(true);
     }
 }
