4862594e28bbf705432fce8419cd35c25777f49e
Matteo Pasqualetti <matteo.pasqualetti@libero.it>
2023-10-13 13:23:01 +0200

    Code cleaning

--- Search-Java/src/main/java/it/unipi/aide/ModelEvaluation.java
+++ Search-Java/src/main/java/it/unipi/aide/ModelEvaluation.java
@@ -1,8 +1,14 @@
 package it.unipi.aide;
 
+/**
+ * This class is used to evaluate the performance of the inverted index
+ */
+
 public class ModelEvaluation {
+
     public static void main( String[] args )
     {
+
         System.out.println("ToDo");
     }
 }
--- Search-Java/src/main/java/it/unipi/aide/QueryHandler.java
+++ Search-Java/src/main/java/it/unipi/aide/QueryHandler.java
@@ -1,5 +1,8 @@
 package it.unipi.aide;
 
+/**
+ * This class is used by the user to make queries on the InvertedIndex
+ */
 public class QueryHandler {
 
     public static void main( String[] args )
