a8a7fe1900ec0ce7d5fa83a829201f88be2052d6
Gabriele Marino <45176656+gabrielemarino-gm@users.noreply.github.com>
2023-11-13 11:18:27 +0100

    Classe per le mie prove

---
+++ Search-Java/src/main/java/it/unipi/aide/GabTest.java
@@ -0,0 +1,11 @@
+package it.unipi.aide;
+
+import it.unipi.aide.utils.ConfigReader;
+
+public class GabTest
+{
+    public static void main(String[] argv)
+    {
+        ConfigReader.stampaDBG();
+    }
+}
