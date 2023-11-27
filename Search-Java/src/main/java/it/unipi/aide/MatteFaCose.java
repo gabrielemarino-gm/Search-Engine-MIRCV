553622607245a0da74a6795f939c1a9957462b3f
MartiB28 <martina.burgisi@gmail.com>
2023-11-27 18:09:02 +0100

    Main Class Developing

--- Search-Java/src/main/java/it/unipi/aide/MatteFaCose.java
+++ Search-Java/src/main/java/it/unipi/aide/MatteFaCose.java
@@ -8,9 +8,7 @@ import java.util.List;
 
 public class MatteFaCose {
 
-
     public static void main(String[] argv) {
-
 
         Preprocesser preprocesser = new Preprocesser(true);
         DAAT daat = new DAAT(10);
@@ -18,81 +16,11 @@ public class MatteFaCose {
         List<String> tokens = preprocesser.process("Folklore tales in the wood");
         long startTime = System.currentTimeMillis();
         System.out.println("Folklore tales in the wood Results:");
-        for(ScoredDocument sd : daat.executeDAAT(tokens)){
+        for (ScoredDocument sd : daat.executeDAAT(tokens)) {
             System.out.print(sd);
         }
         long endTime = System.currentTimeMillis();
         long elapsedTime = endTime - startTime;
-        System.out.println("Tempo impiegato: " + elapsedTime + " millisecondi");
-
-        tokens = preprocesser.process("Folklore tales in the wood");
-        startTime = System.currentTimeMillis();
-        System.out.println("Folklore tales in the wood Results 2:");
-        for(ScoredDocument sd : daat.executeDAAT(tokens)){
-            System.out.print(sd);
-        }
-        endTime = System.currentTimeMillis();
-        elapsedTime = endTime - startTime;
-        System.out.println("Tempo impiegato: " + elapsedTime + " millisecondi");
-
-        tokens = preprocesser.process("Folklore tales");
-        startTime = System.currentTimeMillis();
-        System.out.println("Folklore tales Results 3:");
-        for(ScoredDocument sd : daat.executeDAAT(tokens)){
-            System.out.print(sd);
-        }
-        endTime = System.currentTimeMillis();
-        elapsedTime = endTime - startTime;
-        System.out.println("Tempo impiegato: " + elapsedTime + " millisecondi");
-
-        tokens = preprocesser.process("Folklore tales in the sea");
-        startTime = System.currentTimeMillis();
-        System.out.println("Folklore tales in the sea Results 4:");
-        for(ScoredDocument sd : daat.executeDAAT(tokens)){
-            System.out.print(sd);
-        }
-        endTime = System.currentTimeMillis();
-        elapsedTime = endTime - startTime;
-        System.out.println("Tempo impiegato: " + elapsedTime + " millisecondi");
-
-        tokens = preprocesser.process("English language");
-        startTime = System.currentTimeMillis();
-        System.out.println("English language Results 5:");
-        for(ScoredDocument sd : daat.executeDAAT(tokens)){
-            System.out.print(sd);
-        }
-        endTime = System.currentTimeMillis();
-        elapsedTime = endTime - startTime;
-        System.out.println("Tempo impiegato: " + elapsedTime + " millisecondi");
-
-        tokens = preprocesser.process("English words");
-        startTime = System.currentTimeMillis();
-        System.out.println("English words Results 6:");
-        for(ScoredDocument sd : daat.executeDAAT(tokens)){
-            System.out.print(sd);
-        }
-        endTime = System.currentTimeMillis();
-        elapsedTime = endTime - startTime;
-        System.out.println("Tempo impiegato: " + elapsedTime + " millisecondi");
-
-        tokens = preprocesser.process("English words");
-        startTime = System.currentTimeMillis();
-        System.out.println("English words Results 7:");
-        for(ScoredDocument sd : daat.executeDAAT(tokens)){
-            System.out.print(sd);
-        }
-        endTime = System.currentTimeMillis();
-        elapsedTime = endTime - startTime;
-        System.out.println("Tempo impiegato: " + elapsedTime + " millisecondi");
-
-        tokens = preprocesser.process("Manhattan Project");
-        startTime = System.currentTimeMillis();
-        System.out.println("Manhattan Project Results 8:");
-        for(ScoredDocument sd : daat.executeDAAT(tokens)){
-            System.out.print(sd);
-        }
-        endTime = System.currentTimeMillis();
-        elapsedTime = endTime - startTime;
         System.out.println("Tempo impiegato: " + elapsedTime + " millisecondi");
     }
 }
