0a7f4a7235c50a48033a3e16f19f47e1ff878fe3
Gabriele Marino <45176656+gabrielemarino-gm@users.noreply.github.com>
2023-12-13 17:32:47 +0100

    Le Puttanateeee

--- Search-Java/src/main/java/it/unipi/Driver.java
+++ Search-Java/src/main/java/it/unipi/Driver.java
@@ -10,31 +10,32 @@ import static it.unipi.aide.utils.ColorText.*;
 
 public class Driver
 {
+    // "\t"+YELLOW+"evaluatePerformance:"+ ANSI_RESET + "
     static Scanner scanner = new Scanner(System.in);
-    static String commands = "\tcreateIndex: command useful for create the inverted index from scratch. The comands available are:\n" +
+    static String commands = "\t"+YELLOW+"createIndex:"+ ANSI_RESET + "  command useful for create the inverted index from scratch. The comands available are:\n" +
                              "\t\t\t[-in]:   MANDATORY, indicate the input path, where the corpus is place\n" +
                              "\t\t\t[-ss]:   OPTIONAL, indicate if the stopword and stemming should be applied\n" +
                              "\t\t\t[-d]:    OPTIONAL, indicate if the debug mode should be activated\n" +
                              "\t\t\t[-c]:    OPTIONAL, indicate if the compression should be applied\n\n" +  // TODO: Forse è meglio metterela nel file di configurazione
-                             "\tmakeQuery:   command useful for make a query. After the activation of the command, the application will ask for multiple input query,\n " +
-                             "\t\t\t\t use also the following:\n" +
+                             "\t"+YELLOW+"makeQuery:"+ ANSI_RESET + "  command useful for make a query. After the activation of the command, the application\n " +
+                             "\t\t\t\twill ask for multiple input query, use also the following:\n" +
                              "\t\t\t[s]:   command useful for setup the system.\n" +
                              "\t\t\t[q]:   command useful for exit from the query handler mode.\n" +
                              "\n\n" +
-                             "\tevaluatePerformance:   command useful for evaluate the performance of the model, using the trec_eval tool.\n" +
+                             "\t"+YELLOW+"evaluatePerformance:"+ ANSI_RESET + "  command useful for evaluate the performance of the model, using the trec_eval tool.\n" +
                              "\t\t\t[-in]:   MANDATORY, indicate the input path, where the corpus is place\n" +
                              "\t\t\t[-y]:    OPTIONAL, indicate the year of the trec_eval tool to use, version available: 2019, 2020\n\n" +
-                             "\thelp:   command useful for show the available commands\n\n" +
-                             "\texit:   command useful for exit from the application\n\n";
+                             "\t"+YELLOW+"help:"+ ANSI_RESET + "  command useful for show the available commands\n\n" +
+                             "\t"+YELLOW+"exit:"+ ANSI_RESET + "  command useful for exit from the application\n\n";
     public static void main(String[] args) throws Exception
     {
 
         System.out.println("\n" +
-                "███████ ███████  █████  ██████   ██████ ██   ██     ███████ ███    ██  ██████  ██ ███    ██ ███████ \n" +
-                "██      ██      ██   ██ ██   ██ ██      ██   ██     ██      ████   ██ ██       ██ ████   ██ ██      \n" +
-                "███████ █████   ███████ ██████  ██      ███████     █████   ██ ██  ██ ██   ███ ██ ██ ██  ██ █████   \n" +
-                "     ██ ██      ██   ██ ██   ██ ██      ██   ██     ██      ██  ██ ██ ██    ██ ██ ██  ██ ██ ██      \n" +
-                "███████ ███████ ██   ██ ██   ██  ██████ ██   ██     ███████ ██   ████  ██████  ██ ██   ████ ███████ \n" +
+                BLUE + "███████ "+RED+"███████ "+ YELLOW + " █████  "+ BLUE +"██████  "+ GREEN +" ██████ "+ RED +"██   ██     "+ BLUE +"███████ "+ RED +"███    ██ "+ YELLOW +" ██████  "+ BLUE +"██ "+ GREEN +"███    ██ "+ BLUE +" ███████"+ ANSI_RESET+"\n" +
+                BLUE + "██      "+RED+"██      "+ YELLOW + "██   ██ "+ BLUE +"██   ██ "+ GREEN +"██      "+ RED +"██   ██     "+ BLUE +"██      "+ RED +"████   ██ "+ YELLOW +"██       "+ BLUE +"██ "+ GREEN +"████   ██ "+ BLUE +" ██     "+ ANSI_RESET+"\n" +
+                BLUE + "███████ "+RED+"█████   "+ YELLOW + "███████ "+ BLUE +"██████  "+ GREEN +"██      "+ RED +"███████     "+ BLUE +"█████   "+ RED +"██ ██  ██ "+ YELLOW +"██   ███ "+ BLUE +"██ "+ GREEN +"██ ██  ██ "+ BLUE +" █████  "+ ANSI_RESET+"\n" +
+                BLUE + "     ██ "+RED+"██      "+ YELLOW + "██   ██ "+ BLUE +"██   ██ "+ GREEN +"██      "+ RED +"██   ██     "+ BLUE +"██      "+ RED +"██  ██ ██ "+ YELLOW +"██    ██ "+ BLUE +"██ "+ GREEN +"██  ██ ██ "+ BLUE +" ██     "+ ANSI_RESET+"\n" +
+                BLUE + "███████ "+RED+"███████ "+ YELLOW + "██   ██ "+ BLUE +"██   ██ "+ GREEN +" ██████ "+ RED +"██   ██     "+ BLUE +"███████ "+ RED +"██   ████ "+ YELLOW +" ██████  "+ BLUE +"██ "+ GREEN +"██   ████ "+ BLUE +" ███████"+ ANSI_RESET+"\n" +
                 "                                                                                                    \n\n" +
                 "Welcome to the Search Engine!\n");
         System.out.println("Commands available:");
