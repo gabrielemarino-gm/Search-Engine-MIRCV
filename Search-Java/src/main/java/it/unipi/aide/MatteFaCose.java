374eb38d2b22e39cec9e1e2369832a55aa44d3e2
Matteo <matteo.pasqualetti@libero.it>
2023-12-15 10:22:35 +0100

    Bhoooo

--- Search-Java/src/main/java/it/unipi/aide/MatteFaCose.java
+++ Search-Java/src/main/java/it/unipi/aide/MatteFaCose.java
@@ -1,34 +1,12 @@
 package it.unipi.aide;
 
-import it.unipi.aide.algorithms.ConjunctiveRetrieval;
-import it.unipi.aide.model.PostingListSkippable;
-import it.unipi.aide.model.ScoredDocument;
-import it.unipi.aide.utils.ConfigReader;
-import it.unipi.aide.utils.Preprocesser;
-import it.unipi.aide.utils.QueryPreprocessing;
-import org.apache.commons.compress.utils.Lists;
 
-import java.util.Arrays;
-import java.util.List;
+import me.tongfei.progressbar.ProgressBar;
+import me.tongfei.progressbar.ProgressBarStyle;
+
+import java.io.*;
+
 
 public class MatteFaCose {
-    public static void main(String[] args) {
 
-        Preprocesser p = new Preprocesser(true);
-        ConjunctiveRetrieval cr = new ConjunctiveRetrieval();
-        QueryPreprocessing qp = new QueryPreprocessing();
-
-        for(ScoredDocument s : cr.executeConjunctive(p.process("United States"), true, 10))
-            System.out.print(s);
-        System.out.println("--------------------------------------------------");
-        for(ScoredDocument s : cr.executeConjunctive(p.process("Lucky year"), true, 10))
-            System.out.print(s);
-        System.out.println("--------------------------------------------------");
-        for(ScoredDocument s : cr.executeConjunctive(p.process("United States"), true, 10))
-            System.out.print(s);
-        System.out.println("--------------------------------------------------");
-        for(ScoredDocument s : cr.executeConjunctive(p.process("Lucky year"), true, 10))
-            System.out.print(s);
-
-    }
 }
